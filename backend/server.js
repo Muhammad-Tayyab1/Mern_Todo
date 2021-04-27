9f98564c2b757fde59e91875785941b66ad41d11
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2021-04-27 11:05:37 +0500

    add

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const todoRoutes = express.Router();
+const dotenv= require('dotenv')
+dotenv.config()
 const PORT = 4000;
 
 let Todo = require("./todo.model");
@@ -11,7 +13,7 @@ let Todo = require("./todo.model");
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
+mongoose.connect(process.env.db, { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once("open", function() {
